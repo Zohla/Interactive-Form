b7dc05ace0ee42a4f84997357778bb4cc3735a83
Maren Lilleberre <marenlilleberre@gmail.com>
2019-05-11 00:28:53 +0200

    write t-shirt function
    partly functional. If  color is selected it doesn't hide even if design is deselected (and color not available).

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 
 
-
-
-
-
+/**********PAGE LOAD*******************/
+ 
 
 // Hides other job text input on page load
 $('#toggle_other_job').hide();
@@ -12,7 +10,7 @@ $('#toggle_other_job').hide();
 //selects field by id and focuses the name textfield on page load
 $("#name").focus();
 
-
+/************JOB ROLE*************************/
 // adds function to show text field if 'other' job role is chosen. 
 $("#title").click(function(){
   if  ($('#title').val() === 'other'){
@@ -20,5 +18,29 @@ $("#title").click(function(){
   } else {
   	$('#toggle_other_job').hide();
   }
-
 });
+
+/**************T-SHIRT INFO***************************/
+// const $punShirt = /.*\(JS Puns shirt only\)/;
+// const $heartShirt = /.*\(I &#9829; JS shirt only\)/;
+
+
+
+$("#design").click(function(){
+  if  ($('#design').val() === 'js puns'){
+  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').hide();
+  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
+  	$('#color option[value="cornflowerblue"]').attr('selected', true);
+  } else {
+  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').show();
+  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
+  	$('#color option[value="dimgrey"]').attr('selected', true);
+  }
+});
+
+// $('#design option').filter(function() {
+//     return  /\(JS Puns shirt only\)/.test(this.value);
+// }).remove();
+
+ // /\(JS Puns shirt only\)/ //regEx for pun shirts
+ // /\(I &#9829; JS shirt only\)/ //regEx for love shirts
