ce1bb08edda61dfdd2ddc5462d3e65e3f83b957b
Maren Lilleberre <marenlilleberre@gmail.com>
2019-05-12 08:47:19 +0200

    add function to add cost

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
 
 /**********PAGE LOAD*******************/
- 
+//resets form inputs
+$('form')[0].reset();
 
 // Hides other job text input on page load
 $('#toggle_other_job').hide();
@@ -25,18 +26,49 @@ $("#title").click(function(){
 // const $heartShirt = /.*\(I &#9829; JS shirt only\)/;
 
 
-
+$('#color').hide();//hides colors initially maby change so hides 'color' as well..
 $("#design").click(function(){
   if  ($('#design').val() === 'js puns'){
+  	// $('#color').show();//shows colors - can this be removed?
+  	$('#color option[value="tomato"]').attr('selected', false);
   	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').hide();
   	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
   	$('#color option[value="cornflowerblue"]').attr('selected', true);
   } else {
+  	$('#color option[value="cornflowerblue"]').attr('selected', false);
+  	$('#color').show();//shows colors when design is clicked
   	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').show();
   	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
   	$('#color option[value="dimgrey"]').attr('selected', true);
   }
 });
+
+//makes and appends a new div to show total cost
+$('.activities').append('<div id="totalCost"></div>');
+
+let activityCost = 0;
+
+//event handler to keep track of cost ...so that no activities that collide can be chosen
+$('.activities').change((event)=>{
+	let clicked = event.target;
+	let textContentOfClicked = $(clicked).parent().text();
+	let indexOfCost = textContentOfClicked.indexOf('$');
+	let costOfActivity = textContentOfClicked.slice(indexOfCost);
+	//get price without '$'
+	let costOnlyNum = costOfActivity.slice(1);
+	let costInt = parseInt(costOnlyNum);
+
+	if (clicked.checked) {
+		activityCost += costInt; 
+
+	} else {
+		activityCost -= costInt;
+
+	}
+
+})
+
+
 
 // $('#design option').filter(function() {
 //     return  /\(JS Puns shirt only\)/.test(this.value);
