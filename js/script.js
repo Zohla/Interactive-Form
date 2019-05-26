1a984e6fd3eab31bf6be18d9c3455da66b7e0521
Maren Lilleberre <marenlilleberre@gmail.com>
2019-05-26 09:36:49 +0200

    fix t-shirt select
    added effect to show() and added a choose style-option.
    fixed selector from id to class in validation.

--- js/script.js
+++ js/script.js
@@ -15,31 +15,45 @@ $("#name").focus();
 // adds function to show text field if 'other' job role is chosen. 
 $("#title").click(function(){
   if  ($('#title').val() === 'other'){
-    $('#toggle_other_job').show();
+    $('#toggle_other_job').show(500);
   } else {
   	$('#toggle_other_job').hide();
   }
 });
 
 /**************T-SHIRT INFO***************************/
+const $colorHeading = $('#color').prev(); //gets color heading
+const $choose = $('<option value="choose">Choose Color</option>') //adds an option to the dropdown menu
 
+$('#color').hide();//hides colors initially 
+$colorHeading.hide();//hides color heading
 
-
-$('#color').hide();//hides colors initially maby change so hides 'color' as well..
-$("#design").click(function(){
-  if  ($('#design').val() === 'js puns'){
-  	// $('#color').show();//shows colors - can this be removed?
-  	$('#color option[value="tomato"]').attr('selected', false);
-  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').hide();
-  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
-  	$('#color option[value="cornflowerblue"]').attr('selected', true);
-  } else {
-  	$('#color option[value="cornflowerblue"]').attr('selected', false);
-  	$('#color').show();//shows colors when design is clicked
-  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').show();
-  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
-  	$('#color option[value="dimgrey"]').attr('selected', true);
-  }
+$('#color').prepend($choose);
+$("#design").change(function(){
+	
+	  if  ($('#design').val() === 'js puns'){ 
+	  	$('#color').show(500);//shows colors
+	  	$colorHeading.show(400);
+	  	$('#color option:eq(0)').prop('selected', true);//shows 'choose color' in drop down
+	  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').hide();
+	  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
+	  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').attr('selected', false);
+	  	
+	  	
+	  } else {
+	  	$('#color').hide();
+	  	$colorHeading.hide();
+	  }
+	  if ($('#design').val() === 'heart js'){
+	  	$('#color').show(500);
+	  	$colorHeading.show(400);	
+	  	$('#color option:eq(0)').prop('selected', true);
+	  	$('#color option[value="dimgrey"],[value="steelblue"],[value="tomato"]').show();
+	  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
+	  	$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').attr('selected', false);
+	  	
+	  	
+	  } 
 });
 
 //need to make the initial value in the color drop down go away when design changes................................
@@ -132,7 +146,7 @@ function validName() {
 		return true;
 	} else {
 		name.css('borderColor', 'red');
-		$('#name').prev().append('<span id="incorrect">  You need to enter a valid name.</span>');
+		$('#name').prev().append('<span class="incorrect">  You need to enter a valid name.</span>');
 		return false;
 	}
 }
@@ -143,16 +157,20 @@ function validEmail() {
   const emailResult = emailRegX.test(email);
   if (emailResult === false) {
   	$('#mail').css('borderColor', 'red');
-  	$('#mail').prev().append('<span id="incorrect">  You need to enter a valid email adress.</span>');
+  	$('#mail').prev().append('<span class="incorrect">  You need to enter a valid email adress.</span>');
+  	// return false;
   } else {
-		$('#mail').css('borderColor', '#c1deeb');
-		$('#incorrect').hide();
-	}
+	$('#mail').css('borderColor', '#c1deeb');
+	$('.incorrect').hide();
+	// return true;
+  }
+  
 }
 
-function validActivty() {
+// function validActivty() {
+// 	const 
 
-}
+// }
 
 function validCreditCard() {
 	const cardNumber = $('#cc-num').val();
@@ -160,36 +178,46 @@ function validCreditCard() {
 	const cardResult = cardRegX.test(cardNumber);
 	if (cardResult === false) {
 		$('#cc-num').css('borderColor', 'red');
-		$('#cc-num').prev().append('<span id="incorrect">  Enter valid card number.</span>');
+		$('#cc-num').prev().append('<span class="incorrect">  Enter valid card number.</span>');
+		return false;
 	} else {
 		$('#cc-num').css('borderColor', '#c1deeb');
-		$('#incorrect').hide();
+		$('.incorrect').hide();
+		return true;
 	}
 	const zipCode = $('#zip').val();
 	const zipRegX = /\d{5}/;
 	const zipResult = zipRegX.test(zipCode);
 	if (zipResult === false) {
 		$('#zip').css('borderColor', 'red');
-		$('#zip').prev().append('<span id="incorrect">Incorrect.</span>');
+		$('#zip').prev().append('<span class="incorrect">Incorrect.</span>');
+		return false;
 	} else {
 		$('#zip').css('borderColor', '#c1deeb');
-		$('#incorrect').remove();
+		$('.incorrect').hide();
+		return true;
 	}
 	const cvv = $('#cvv').val();
 	const cvvRegX = /\d{3}/;
 	const cvvResult = cvvRegX.test(cvv);
 	if (cvvResult === false) {
 		$('#cvv').css('borderColor', 'red');
-		$('#cvv').prev().append('<span id="incorrect">Incorrect.</span>');
+		$('#cvv').prev().append('<span class="incorrect">Incorrect.</span>');
+		return false;
 	} else {
 		$('#cvv').css('borderColor', '#c1deeb');
-		$('#incorrect').hide();
+		$('.incorrect').hide();
+		return true;
 	}
 }
 
+// function validateForm() {
+// 	let validForm = true;
+// 	if 
+// }
 $('button').click((event)=>{
 	event.preventDefault();
-	$('#incorrect').remove();// not working, keeps adding.................................................
+	$('.incorrect').remove();
 	validName();
 	validEmail();
 	validCreditCard();
